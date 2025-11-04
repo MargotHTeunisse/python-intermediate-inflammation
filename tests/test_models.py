198f9d80d718ae01ed65d7dc5d4464fce42a1871
Margot Teunisse <margotteunisse@hotmail.com>
2025-11-04 11:27:08 +0100

    Added patient normalization function and testing.

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,7 +4,7 @@ import numpy as np
 import numpy.testing as npt
 import pytest
 
-from inflammation.models import daily_mean, daily_max, daily_min
+from inflammation.models import daily_mean, daily_max, daily_min, patient_normalise
 
 @pytest.mark.parametrize(
         "test, expected",
@@ -41,6 +41,18 @@ def test_daily_max(test, expected):
 def test_daily_min(test, expected):
     """Test mean function works for array of zeroes and positive integers."""
     npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]])
+    ])
+def test_patient_normalise(test, expected):
+    """Test normalisation works for arrays of one and positive integers.
+       Test with a relative and absolute tolerance of 0.01."""
+
+    result = patient_normalise(np.array(test))
+    npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
 
 def test_daily_min_string():
     """Test for TypeError when passing strings"""
